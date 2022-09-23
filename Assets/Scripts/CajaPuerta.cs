434ef5be646eaf650fdc10630ed9c6697bd2ac3d
renedsoft <renedsoft@gmail.com>
2022-09-23 12:38:19 -0400

    mecanica 01 v0.1 b

--- Assets/Scripts/CajaPuerta.cs
+++ Assets/Scripts/CajaPuerta.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//Esto no se debe pasar a Caja controlador
+//pues caja controlador tiene otro collider para los toques 
+
 public class CajaPuerta : MonoBehaviour {
 
     //Esfera en el centro de la caja, para pasar sus coordenadas al gato
@@ -10,9 +14,17 @@ public class CajaPuerta : MonoBehaviour {
     //Caja para poder saber la orientacion de la caja
     public Transform Caja;
 
+
+    //Direccion de la puerta de la caja, obtenido desde el parent
+    public DireccionObjetos direccion_puerta_caja = DireccionObjetos.Sur;
+
     void Start () {
 
         // mascara_colisiones = LayerMask.GetMask ("gato");
+    }
+
+    void Update() {
+        direccion_puerta_caja = Caja.GetComponent<CajaControlador>().direccion_puerta_caja;
     }
 
 }
